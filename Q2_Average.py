f0b0489e6d22d711dfa24a4d0b204b9d0fde085b
ChandraBChoudhary <94103997+ChandraBChoudhary@users.noreply.github.com>
2021-11-19 13:04:40 +0530

    Update Q2_Average.py

--- Q2_Average.py
+++ Q2_Average.py
@@ -3,6 +3,6 @@ total = 0
 denominator = len(marks)
 for ele in range(0, denominator):
     total = total + marks[ele]
-    Average_marks = total / denominator
+Average_marks = total / denominator
     
 print("Average marks scored by student is: " , Average_marks)
